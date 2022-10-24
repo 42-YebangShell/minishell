9e996fd25b0007988f2ee123a6c716a6487508fa
yebeen <yebeen79@naver.com>
2022-10-24 11:09:31 +0900

    execve 에서 값을 찾지 못하는 오류 확인 필요

--- srcs/utils/util_redirection.c
+++ srcs/utils/util_redirection.c
@@ -1,17 +1,3 @@
 #include "../../includes/minishell.h"
 
-int	redir_check_here_doc(t_info *info)
-{
-	t_token	*token;
-
-	signal(SIGINT, &sig_here_doc);
-	token = info->h_token;
-	while (token)
-	{
-		if (token->type == HERE_DOC)
-			if (!redir_here_doc(token->next))
-				return (FALSE);
-		token = token->next;
-	}
-	return (TRUE);
-}
+// ß
